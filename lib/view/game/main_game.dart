6096eca253868a0e7ca0ae8f31c0a3b3b20cd64a
Naga <nagachiangdev@gmail.com>
2024-02-11 19:34:18 +0800

    Remove Tiled component

--- lib/view/game/main_game.dart
+++ lib/view/game/main_game.dart
@@ -2,14 +2,9 @@ import 'dart:async';
 
 import 'package:flame/game.dart';
 
-import '../map/map_component.dart';
-
 class MainGame extends FlameGame {
   @override
   Future<void> onLoad() async {
     super.onLoad();
-
-    final map = MapComponent();
-    add(map);
   }
 }
